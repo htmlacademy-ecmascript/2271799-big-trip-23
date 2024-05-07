89a53fb90d951670f3605d3df6168cf0cf937fd8
valeriyasv <lerushka.sav@gmail.com>
2024-05-06 23:56:38 -0500

    adds mock data

---
+++ src/const.js
@@ -0,0 +1,4 @@
+const CITIES = ['Amsterdam', 'New York', 'Samara', 'Mexico'];
+const TYPES = ['Taxi', 'Bus', 'Train', 'Ship', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
+
+export {CITIES, TYPES};
---
+++ src/mock/offers.js
@@ -0,0 +1,37 @@
+const offers = [
+  {
+    type: 'taxi',
+    offers: [
+      {
+        id: 'b4c3e4e6-9053-42ce-b747-e281314baa31',
+        title: 'Upgrade to a business class',
+        price: 120
+      }]
+  },
+  {
+    type: 'train',
+    offers: [
+      {
+        id: 'b4c3e4e6-9053-42ce-b747-e281314baa31',
+        title: 'Upgrade to a business class',
+        price: 120
+      }]
+  },
+  {
+    type: 'bus',
+    offers: [
+      {
+        id: 'b4c3e4e6-9053-42ce-b747-e281314baa31',
+        title: 'Upgrade to a business class',
+        price: 120
+      },
+      {
+        id: 'b4c3e4e6-9053-42ce-b747-e281314baa31',
+        title: 'Upgrade to a business class',
+        price: 170
+      }
+    ]
+  }
+];
+
+export {offers};
