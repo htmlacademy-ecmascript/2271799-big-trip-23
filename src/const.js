f8e3ae5700c0f09b3513ad855eddd1a8dc57e433
valeriyasv <lerushka.sav@gmail.com>
2024-05-25 12:31:04 -0500

    adds filter data structure

--- src/const.js
+++ src/const.js
@@ -1,4 +1,11 @@
 const CITIES = ['Chamonix', 'New York', 'Samara'];
 const TYPES = ['Taxi', 'Bus', 'Train', 'Ship', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
 
-export {CITIES, TYPES};
+const FilterType = {
+  EVERYTHING: 'Everything',
+  FUTURE: 'Future',
+  PRESENT: 'Present',
+  PAST: 'Past',
+};
+
+export {CITIES, TYPES, FilterType};
